6e6c35d94d14e121a1ba98c8d3609c3aba72af85
koallen <weifengzi2009@gmail.com>
2016-08-26 01:04:44 +0800

    add rest framework

--- eclinic/eclinic/settings.py
+++ eclinic/eclinic/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'user_registration',
+    'user_auth'
 ]
 
 MIDDLEWARE = [
