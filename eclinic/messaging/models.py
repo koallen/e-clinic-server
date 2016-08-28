32a21d3427b07ff2ec403109ee7ba660603b2737
koallen <weifengzi2009@gmail.com>
2016-08-28 16:35:19 +0800

    fix POST /tokens/

--- eclinic/messaging/models.py
+++ eclinic/messaging/models.py
@@ -3,7 +3,5 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class MessageToken(models.Model):
-	token = models.CharField(max_length=200)
-	username = models.ForeignKey(User)
-
-
+    token = models.CharField(max_length=200)
+    user = models.ForeignKey(User, to_field="username")
--- eclinic/messaging/serializers.py
+++ eclinic/messaging/serializers.py
@@ -3,32 +3,28 @@ from django.contrib.auth.models import User
 from .models import MessageToken
 
 class MessageSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = MessageToken
-		fields = ('token', 'username')
 
-	
-	def create(self, **validated_data):
-		"""
-		create a message token for an existing user
-		"""
-		try:
-			MessageToken.objects.get(username = validated_data.get('username'))
-			
-		except MessageToken.DoesNotExist:
-			return MessageToken.objects.create(**validated_data)
-			
-		return False
-
-	def update(self, instance, **validated_data):
-		"""
-    	update a message token
-    	"""
-		instance.token = validated_data.get('token', instance.token)
-		instance.save()
-		return True
+    class Meta:
+        model = MessageToken
 
 
+    def create(self, validated_data):
+        """
+        create a message token for an existing user
+        """
+        username = validated_data.get('username')
+        token = validated_data.get('token')
+        try:
+            MessageToken.objects.get(user=username)
+        except MessageToken.DoesNotExist:
+            return MessageToken.objects.create(**validated_data)
 
+        return False
 
-
+    def update(self, instance, validated_data):
+        """
+        update a message token
+        """
+        instance.token = validated_data.get('token', instance.token)
+        instance.save()
+        return True
