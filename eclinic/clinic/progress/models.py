7fa4e0e68a8deda69e59217fd7da0e8e71eb9bb3
iamqingmei <iamqingmei@gmail.com>
2016-08-28 17:26:04 +0800

    progress

--- eclinic/clinic/progress/models.py
+++ eclinic/clinic/progress/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Progress(models.Model):
+    doctor = models.ForeignKey(User, to_field="username")
+    patient = models.ForeignKey(User, to_field="username")
+    content = models.CharField(max_length=500)
+    dateTime = models.DateTimeField()
+    
--- eclinic/messaging/models.py
+++ eclinic/messaging/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 # Create your models here.
 class MessageToken(models.Model):
     token = models.CharField(max_length=200)
-    user = models.ForeignKey(User, to_field="username")
+    user = models.OneToOneField(User, to_field="username")
