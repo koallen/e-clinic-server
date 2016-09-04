858d7ea73f12efdda2035e641bdd8db66d5fdab1
iamqingmei <iamqingmei@gmail.com>
2016-09-04 11:59:51 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	eclinic/messaging/views.py

--- eclinic/messaging/views.py
+++ eclinic/messaging/views.py
@@ -35,6 +35,14 @@ class MessageTokenDetail(APIView):
             return Response(serializer.data, status = status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request, id, format=None):
+        """
+        delete a message token
+        """
+        messageTokenToDelete = self.get_object(id)
+        messageTokenToDelete.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 class MessageList(APIView):
 
     def post(self, request, format=None):
