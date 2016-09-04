2d9beea474f193acc8a7d401481f22d01f782cb4
koallen <weifengzi2009@gmail.com>
2016-09-04 11:36:53 +0800

    fix model issues

--- eclinic/clinic/reservation/models.py
+++ eclinic/clinic/reservation/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from user_registration.models import Doctor, Patient
 
 # Create your models here.
+class Reservation(models.Model):
+   """
+   Reservation to visit a doctor
+   """
+   doctor = models.ForeignKey(Doctor, to_field="user")
+   patient = models.ForeignKey(Patient, to_field="user")
+   datetime = models.DateTimeField()
--- eclinic/user_registration/models.py
+++ eclinic/user_registration/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Doctor(models.Model):
+    """
+    Doctor information
+    """
+    user = models.OneToOneField(User, to_field="username", related_name="doctor")
+    clinic = models.CharField(max_length=100)
+    gender = models.CharField(max_length=1)
+    description = models.CharField(max_length=500)
+
+class Patient(models.Model):
+    """
+    Patient information
+    """
+    user = models.OneToOneField(User, to_field="username", related_name="patient")
+    gender = models.CharField(max_length=1)
+    age = models.IntegerField()
